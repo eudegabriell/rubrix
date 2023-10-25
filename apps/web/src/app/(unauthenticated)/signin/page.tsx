96e06dce8c63a703a7658a806414af35fc62ebcc
Eude Gabriel <33588952+eudegabriell@users.noreply.github.com>
2023-10-25 09:50:08 -0300

    Update page.tsx

--- apps/web/src/app/(unauthenticated)/signin/page.tsx
+++ apps/web/src/app/(unauthenticated)/signin/page.tsx
@@ -5,18 +5,18 @@ import { SignInForm } from '~/components/forms/signin';
 export default function SignInPage() {
   return (
     <div>
-      <h1 className="text-4xl font-semibold">Sign in to your account</h1>
+      <h1 className="text-4xl font-semibold">Faça login em sua conta</h1>
 
       <p className="text-muted-foreground/60 mt-2 text-sm">
-        Welcome back, we are lucky to have you.
+        Bem-vindo de volta!
       </p>
 
       <SignInForm className="mt-4" />
 
       <p className="text-muted-foreground mt-6 text-center text-sm">
-        Don't have an account?{' '}
+        Não tem uma conta?{' '}
         <Link href="/signup" className="text-primary duration-200 hover:opacity-70">
-          Sign up
+          Cadastrar-se
         </Link>
       </p>
 
@@ -25,7 +25,7 @@ export default function SignInPage() {
           href="/forgot-password"
           className="text-muted-foreground text-sm duration-200 hover:opacity-70"
         >
-          Forgotten your password?
+          Esqueceu sua senha?
         </Link>
       </p>
     </div>
